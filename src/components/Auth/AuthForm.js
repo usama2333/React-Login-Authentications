1326c400dd0d1b293fe8616032df43e6df9f1b8e
usama2333 <usama.wizz@gmail.com>
2023-03-02 16:26:46 +0500

    login signup Authentication works

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,28 +1,93 @@
-import { useState } from 'react';
+import { useState , useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading , setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true);
+    let url;
+    if(isLogin) {
+
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBmKQ8zia8XJY4OT1Y05J6XVfvn3_LO86k';
+
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBmKQ8zia8XJY4OT1Y05J6XVfvn3_LO86k';
+      
+    }
+
+    fetch( url,
+        {
+          method : 'POST',
+          body : JSON.stringify({
+
+            email : enteredEmail,
+            password : enteredPassword,
+            returnSecureTocken : true
+
+          }),
+
+          headers : {
+            'Content-Type' : 'application/json'
+          }
+        }
+      ).then((response) => {
+
+        setIsLoading(false);
+
+        if(response.ok) {
+          return response.json();
+          /////
+        } else {
+          return response.json().then((data) => {
+              
+           let errorMessage = 'Authentication Failed';
+          //  if(data && data.error && data.error.message) {
+          //   errorMessage = data.error.message;
+          //  }
+
+           
+           throw new Error(errorMessage);
+
+          });
+        }
+      }).then((data) => {
+
+      }).catch((error) => {
+        alert(error.message);
+      })
+
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' ref={emailInputRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' ref={passwordInputRef} required />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Is Loading .....</p>}
           <button
             type='button'
             className={classes.toggle}
